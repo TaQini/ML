4c9af36d874015f7ba1e3a21110248c51a3657b4
TaQini <742954809@qq.com>
2018-01-22 22:33:37 +0800

    init WSN ep3  - writen some script to sample data

---
+++ wsn_ep3/tools/sample_with_label.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+#for A,B,C,D,E
+from sys import argv
+__author__ = 'TaQini'
+
+if len(argv) != 3:
+	print 'usage: '
+	print ' $ sample.py <feature_file> <label_file>'
+	print 'example: '
+	print ' $ sample.py A.feature A.label'
+	exit(0)
+
+feature_file  = open(argv[1],'ro')
+label_file = open(argv[2],'ro')
+feature_sample_file = open(argv[1]+'.sample','w+')
+label_sample_file = open(argv[2]+'.sample','w+')
+
+# read data
+fdata = feature_file.readlines()
+ldata = label_file.readlines()
+feature_file.close()
+label_file.close()
+
+# del all item having '?'
+feature = []
+label = []
+for item in range(len(fdata)):
+	if '?' not in fdata[item]:
+		feature.append(fdata[item])
+		label.append(ldata[item])
+
+feature_sample_file.writelines(feature)
+feature_sample_file.close()
+
+label_sample_file.writelines(label)
+label_sample_file.close()
---
+++ wsn_ep3/tools/sample_without_sample.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+#for X,Y,Z
+from sys import argv
+__author__ = 'TaQini'
+
+if len(argv) != 2:
+	print 'usage: '
+	print ' $ sample.py <feature_file>'
+	print 'example: '
+	print ' $ sample.py X.feature'
+	exit(0)
+
+feature_file  = open(argv[1],'r')
+sample_file = open(argv[1]+'.sample','w+')
+
+# read data
+data = feature_file.readlines()
+feature_file.close()
+
+# del all item having '?'
+result = []
+for item in data:
+	if '?' not in item:
+		result.append(item)
+
+sample_file.writelines(result)
+sample_file.close()
