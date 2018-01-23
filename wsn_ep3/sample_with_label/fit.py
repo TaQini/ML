9a5d431084e7ed037b9b4b30fc486bcbf8d5e076
TaQini <742954809@qq.com>
2018-01-24 00:19:01 +0800

    fit by DT  - ep3 80%  - result: XYZ.label

---
+++ wsn_ep3/sample_with_label/fit.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import numpy as np
+from sys import argv
+from sklearn import tree
+
+feature_file = open(argv[1],'ro')
+label_file = open(argv[2],'ro')
+
+X = np.array([eval(item)[1:] for item in feature_file.readlines()])
+Y = np.array([eval(item) for item in label_file.readlines()])
+
+feature_file.close()
+label_file.close()
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, Y)
+
+predict_file = open(argv[3],'ro')
+# real_label = open(argv[4],'ro')
+predict_lable = open(argv[4],'w+')
+
+pX = np.array([eval(item)[1:] for item in predict_file.readlines()])
+#rY = np.array([eval(item) for item in real_label.readlines()])
+
+predict_file.close()
+#real_label.close()
+
+pY = clf.predict(pX)
+for i in pY:
+	predict_lable.write(str(i)+'\n')
+
+predict_lable.close()
+
+#cnt = 0
+#for i in range(len(pY)):
+#	if rY[i] == pY[i]:
+#		cnt += 1
+#print 'rate: '+str(cnt*100.0/len(Y))+'%'
+
